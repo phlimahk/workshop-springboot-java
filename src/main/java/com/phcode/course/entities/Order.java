bab3de19b6b8aa4cb35de2c90e241a27b0313d2b
Raphael Lima <phlimahk@gmail.com>
2019-10-20 20:44:01 -0300

    Order Enums

--- src/main/java/com/phcode/course/entities/Order.java
+++ src/main/java/com/phcode/course/entities/Order.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.phcode.course.entities.enums.OrderStatus;
 
 
 @Entity
@@ -26,6 +26,7 @@ public class Order implements Serializable{
 	private Long id;
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T'HH:mm:ss 'Z'",timezone = "GMT")
 	private Instant moment;
+	private Integer orderStatus;
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
@@ -33,10 +34,11 @@ public class Order implements Serializable{
 	public Order() {
 	}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus,User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -87,6 +89,16 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus!=null) {
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 	
 	
