0be1e19d7b16d23426a9f98557ff671dc009d11e
Raphael Lima <phlimahk@gmail.com>
2019-10-21 21:32:11 -0300

    ManyToMany JoinTable

--- src/main/java/com/phcode/course/entities/Category.java
+++ src/main/java/com/phcode/course/entities/Category.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 @Table(name="tb_category")
@@ -22,6 +24,7 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
     @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
 	private Set<Product> products = new HashSet<>();
 
 
